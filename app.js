d2500523dedcb65d9c6679948da4b41344cb7bce
Usman Husnan <oets.man@gmail.com>
2022-09-11 14:18:30 +0700

    init

---
+++ app.js
@@ -0,0 +1,2 @@
+import main from "./src/script/view/main";
+document.addEventListener("DOMContentLoaded", main);
---
+++ src/script/data/data-source.js
@@ -0,0 +1,46 @@
+import clubs from "./clubs";
+class DataSource {
+  static searchClub = function (keyword) {
+    return new Promise((success, fail) => {
+      const filteredClubs = clubs.filter((club) =>
+        club.name.toUpperCase().includes(keyword.toUpperCase())
+      );
+
+      if (filteredClubs.length) {
+        success(filteredClubs);
+      } else {
+        fail(`${keyword} is not found`);
+      }
+    });
+  };
+}
+
+export default DataSource;
+
+// function DataSource(onSuccess, onFailed) {
+//   this.onSuccess = onSuccess;
+//   this.onFailed = onFailed;
+// }
+
+// // DataSource.prototype.searchClub = function (keyword) {
+// //   const filteredClubs = clubs.filter(function (club) {
+// //     return club.name.toUpperCase().includes(keyword.toUpperCase());
+// //   });
+
+// //   if (filteredClubs.length) {
+// //     this.onSuccess(filteredClubs);
+// //   } else {
+// //     this.onFailed(`${keyword} is not found`);
+// //   }
+// // };
+// DataSource.prototype.searchClub = function (keyword) {
+//   const filteredClubs = clubs.filter((club) =>
+//     club.name.toUpperCase().includes(keyword.toUpperCase())
+//   );
+
+//   if (filteredClubs.length) {
+//     this.onSuccess(filteredClubs);
+//   } else {
+//     this.onFailed(`${keyword} is not found`);
+//   }
+// };
