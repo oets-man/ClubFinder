0a01f5894fd33bf3a28895f6c9bcd117a9bb5d75
Usman Husnan <oets.man@gmail.com>
2022-09-15 01:32:25 +0700

    implemen shadow pada club finder

--- src/component/club-list.js
+++ src/component/club-list.js
@@ -1,23 +1,37 @@
 import "./club-item.js";
 
 class ClubList extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
   set clubs(clubs) {
     this._clubs = clubs;
     this.render();
   }
 
   render() {
-    this.innerHTML = "";
+    this.shadow.innerHTML = `
+      <style>
+      .placeholder {
+        font-weight: lighter;
+        color: rgba(0, 0, 0, 0.5);
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+      </style>`;
     this._clubs.forEach((club) => {
       const clubItemElement = document.createElement("club-item");
       clubItemElement.club = club;
-      this.appendChild(clubItemElement);
+      this.shadow.appendChild(clubItemElement);
     });
   }
 
   renderError(message) {
-    this.innerHTML = "";
-    this.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+    this.shadow.innerHTML = "";
+    this.shadow.innerHTML += `<h2 class="placeholder">${message}</h2>`;
   }
 }
 customElements.define("club-list", ClubList);
--- src/component/search-bar.js
+++ src/component/search-bar.js
@@ -1,10 +1,76 @@
 class SearchBar extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
   connectedCallback() {
     this.render();
   }
 
   render() {
-    this.innerHTML = `
+    this.shadow.innerHTML = `
+        <style>
+        .search-container {
+          max-width: 800px;
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+          padding: 16px;
+          border-radius: 5px;
+          display: flex;
+          position: sticky;
+          top: 10px;
+          background-color: white;
+        }
+
+        .search-container > input {
+          width: 75%;
+          padding: 16px;
+          border: 0;
+          border-bottom: 1px solid cornflowerblue;
+          font-weight: bold;
+        }
+
+        .search-container > input:focus {
+          outline: 0;
+          border-bottom: 2px solid cornflowerblue;
+        }
+
+        .search-container > input:focus::placeholder {
+          font-weight: bold;
+        }
+
+        .search-container > input::placeholder {
+          color: cornflowerblue;
+          font-weight: normal;
+        }
+
+        .search-container > button {
+          width: 23%;
+          cursor: pointer;
+          margin-left: auto;
+          padding: 16px;
+          background-color: cornflowerblue;
+          color: white;
+          border: 0;
+          text-transform: uppercase;
+        }
+
+        @media screen and (max-width: 550px) {
+          .search-container {
+            flex-direction: column;
+            position: static;
+          }
+
+          .search-container > input {
+            width: 100%;
+            margin-bottom: 12px;
+          }
+
+          .search-container > button {
+            width: 100%;
+          }
+        }
+        </style>
+
         <div id="search-container" class="search-container">
         <input
           placeholder="Search football club"
@@ -14,10 +80,9 @@ class SearchBar extends HTMLElement {
         <button id="searchButtonElement" type="submit">Search</button>
       </div>`;
 
-    this.querySelector("#searchButtonElement").addEventListener(
-      "click",
-      this._clickEvent
-    );
+    this.shadow
+      .querySelector("#searchButtonElement")
+      .addEventListener("click", this._clickEvent);
   }
 
   set clickEvent(event) {
@@ -26,7 +91,7 @@ class SearchBar extends HTMLElement {
   }
 
   get value() {
-    return this.querySelector("#searchElement").value;
+    return this.shadow.querySelector("#searchElement").value;
   }
 }
 
